048e597585a296bf227fc70419f28349b7364cb0
Elias Kuiter <info@elias-kuiter.de>
2014-12-19 12:21:33 +0100

    Initial Commit

---
+++ ledcube4-test/ledcube4-test.ino
@@ -0,0 +1,21 @@
+void setup() {
+  for (int i = 0; i < 24; i++)
+    pinMode(i, OUTPUT);
+}
+
+void loop() {
+  for (int i = 18; i < 22; i++) {
+    for (int i = 0; i < 24; i++)
+     digitalWrite(i, LOW);
+    digitalWrite(i, HIGH);
+    for (int i = 0; i < 14; i++) {
+      digitalWrite(i, HIGH);
+      delay(100);
+    }
+    digitalWrite(23, HIGH);
+    delay(100);
+    digitalWrite(22, HIGH);
+    delay(100);
+  }
+}
+
